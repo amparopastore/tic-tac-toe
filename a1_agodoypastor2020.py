46dc810c766df1fd4002e85bd3795176a114763f
Amparo Godoy Pastore <96999371+amparopastore@users.noreply.github.com>
2023-06-05 18:03:54 -0400

    Add files via upload

---
+++ a1_agodoypastor2020.py
@@ -0,0 +1,13 @@
+# Intro to Artificial Intelligence
+# Project 1: Tic Tac Toe
+# Author: Amparo Godoy Pastore
+# Date: 5/30/2023
+
+print("TIC-TAC-TOE\n"
+      "This app is an implementation of a solution to a classical AI problem: playing the game of tic-tac-toe." 
+      "We implement adversarial search, particularly MINMAX")
+
+print("| 0 | 1 | 2 |\n| 3 | 4 | 5 |\n| 6 | 7 | 8 |")
+
+# MINMAX search algorithm is implemented to be a recursive function
+# 
