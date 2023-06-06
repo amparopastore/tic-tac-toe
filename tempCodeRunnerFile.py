71bd334c2a893a4f5d880dc035c4878187e8d1d5
Amparo Godoy Pastore <agodoypastore@gmail.com>
2023-06-05 22:30:53 -0400

    gone

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-      print('|', board[0], '|', board[1], '|', board[2], '|')
-      print('|', board[3], '|', board[4], '|', board[5], '|')
-      print('|', board[6], '|', board[7], '|', board[8], '|')
